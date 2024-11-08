6b92dbde35646d6d693baaf7f0e2a549ad0f067b
Andrew Klimkov <andrew.klimkov@innowise.com>
2024-11-08 15:34:22 +0300

    feat: add new links to xlxs table

--- src/components/TableLink/TableLink.tsx
+++ src/components/TableLink/TableLink.tsx
@@ -1,7 +1,7 @@
 export const TableLink = () => {
   return (
     <a
-      href="https://docs.google.com/spreadsheets/d/1kRrHkQZnbyGp1wyuL6qyLvLjB4trMa2i4dx8tObNVAE/edit#gid=0"
+      href="https://docs.google.com/spreadsheets/d/1pW1AU5Snu4TVtCy3K26J0z7LSqVf5eDima-NaGLS0e8/edit?gid=0#gid=0"
       target="_blank"
     >
       Table
